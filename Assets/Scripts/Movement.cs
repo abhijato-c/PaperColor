77da0e5cc194315b75bf1628e34f143f9286401d
abhijato-c <abhijato.c@gmail.com>
2026-06-20 19:14:51 -0400

    Finish lvl 1

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,12 +1,13 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour {
     public Transform GroundCheck;
     public LayerMask GroundLayer;
     public float MoveSpeed = 5f;
     public float JumpForce = 12f;
-    public float FallForce = 2.5f;
 
     private IInteractable Interactable; 
     private bool InteractInput => Keyboard.current.eKey.wasPressedThisFrame;
@@ -46,12 +47,9 @@ public class Movement : MonoBehaviour {
         anim.SetFloat("Speed", horizontalSpeed);
 
         if (Jumping) {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpForce);
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
             Jumping = false;
         }
-
-        if (rb.linearVelocity.y < 0)
-            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (FallForce - 1) * Time.fixedDeltaTime;
 
         if (input > 0 && !FacingForward || input < 0 && FacingForward)
             FlipCharacter();
@@ -68,6 +66,10 @@ public class Movement : MonoBehaviour {
         IInteractable interactable = collision.GetComponent<IInteractable>();
         if (interactable != null) {
             Interactable = interactable;
+        }
+        if (collision.CompareTag("Respawn")) {
+            string NextLvl = "lv" + (int.Parse(SceneManager.GetActiveScene().name[2..]) + 1);
+            SceneManager.LoadScene(NextLvl);
         }
     }
 
