9177dfdf7cbb526fd2cd7c8fb99cbe3472158667
Sreejith <exetra63@gmail.com>
2026-06-21 01:48:41 -0400

    Finish hover

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,8 +1,23 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class Finish : MonoBehaviour
 {
+    public float amplitude;
+    public float period;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = gameObject.transform.position;
+    }
+
+    void Update() {
+        gameObject.transform.position = startPos + Vector3.up * (float)(amplitude * Math.Sin(2.0f * Math.PI / period * Time.fixedUnscaledTime));
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
