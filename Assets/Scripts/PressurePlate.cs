af671a3879134c13fc9d4d6cc85ffe492c9ec0e4
abhijato-c <abhijato.c@gmail.com>
2026-06-21 01:04:48 -0400

    Add phys mat

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -29,8 +29,7 @@ public class PressurePlate : MonoBehaviour{
         // check if any ghosts are still on it
         List<Collider2D> overlaps = new List<Collider2D>();
         GetComponent<Collider2D>().Overlap(ContactFilter2D.noFilter, overlaps);
-        foreach (Collider2D collider in overlaps)
-        {
+        foreach (Collider2D collider in overlaps) {
             if (collider.gameObject.CompareTag("ghost")) { return; }
         }
 
